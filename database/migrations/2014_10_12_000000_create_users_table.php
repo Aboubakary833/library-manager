3eae084ff82672beb45fee7f9f16c3f848ac3df0
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-11-23 15:18:28 +0000

    chore: Started working on migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->ulid("id")->primary();
+            $table->foreignUlid("library_id")->references("id")->on("libraries")->cascadeOnDelete();
             $table->string('firstname');
             $table->string('lastname');
             $table->string("username")->unique();
