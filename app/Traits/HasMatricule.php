bfadcc95e8fef1a62f07b676bdcbbdcfe147815d
Aboubakary833 <aboubakarycisse472@gmail.com>
2023-11-29 19:38:08 +0000

    wip: HasMatricule trait

--- app/Traits/HasMatricule.php
+++ app/Traits/HasMatricule.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
+use Exception;
+
 trait HasMatricule {
 
-	protected $prefix = "";
+	// protected $prefix = "";
 
-	protected $length = 5;
+	// protected $length = 5;
+
+	public static function bootHasMatricule() {
+		static::creating(function($model) {
+			if($model->length > 255) {
+				throw new Exception("Too big matricule lenght: Must be less than 255 characters.");
+			}
+		});
+	}
+
+	// public static function generate() : string {
+	// 	return 
+	// }
 
 }
--- database/migrations/2023_11_25_150535_create_publishing_houses_table.php
+++ database/migrations/2023_11_25_150535_create_publishing_houses_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('publishing_houses', function (Blueprint $table) {
             $table->ulid("id")->primary();
             $table->string("name");
-            $table->
             $table->timestamps();
         });
     }
